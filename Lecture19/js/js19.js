76d781f4e9191b2791942d52da88a79a2c31cebe
shujamughal <shujamughal@gmail.com>
2020-08-21 16:50:12 +0500

    lecture 19 and 20

---
+++ Lecture19/js/js19.js
@@ -0,0 +1,55 @@
+
+//number
+
+var x = 5;
+console.log(x);
+
+console.log(typeof(x) );
+x= 54.56;
+console.log(typeof(x) );
+
+//string
+
+var y = "AOA Pakistan";
+console.log(typeof(y) );
+
+var z = 'AOA Pakistan';
+console.log(typeof(z) );
+
+//boolean
+var a= true;
+var b=false;
+
+console.log(typeof(a) );
+console.log(typeof(b) );
+
+//null;
+
+var c = null;
+console.log(c);
+//undefined
+var d;
+console.log(d);
+
+//object
+
+var person = {
+				firstName : "shuja",
+				lastName: "baig"
+};
+
+console.log(person);
+console.log(person.firstName);
+
+var person2 = {
+				firstName : "shuja",
+				lastName: "baig",
+				contactInformation: {
+									city: "Lahore",
+									country: "Pakistan"
+									}
+};
+console.log(person2);
+console.log(person2.contactInformation.city);
+
+
---
+++ Lecture20/js/js20.js
@@ -0,0 +1,96 @@
+
+// // //string concatination
+// // var myVariable = "AoA ";
+
+// // //myVariable = myVariable + "Pakistan";
+// // myVariable +=  "Pakistan";
+// // console.log(myVariable +  " !");
+
+
+
+
+
+
+// // //regular math operations (+,-, *, /)
+// // console.log( (3+6)/3);
+// // console.log(undefined/34);
+
+
+
+
+
+
+
+
+// // //equal operation
+
+// var x=2, y=2;
+
+// if(x == y)
+// {
+	// console.log("x and y are equal")
+// }else{
+	
+	// console.log("x and y are NOT equal")
+// }
+
+// x="2";
+// if(x == y)
+// {
+	// console.log("x and y are equal")
+// }else{
+	
+	// console.log("x and y are NOT equal")
+// }
+
+// // //strictly equal operation
+
+// if(x === y)
+// {
+	// console.log("x and y are equal")
+// }else{
+	
+	// console.log("x and y are NOT equal")
+// }
+
+// // //what is false in javascript?
+
+// if(false || 0 || null ||
+ // undefined || "" || NaN)
+	// {
+// console.log("this line will never execute");
+	// }else {
+		// console.log("All False");
+		
+	// }
+// // //what is true in javascript?	
+// if(true && 1&& -1 && "hello" && "false")
+	// {
+		// console.log("All True");
+	// }else {
+		// console.log("this line will never execute");
+		
+	// }	
+	
+// function a(){
+	// return		{
+		// name: "baig"
+	// };
+// }	
+
+// function b() {
+	// return 	{
+		// name: "baig"
+	// };
+// }	
+
+// console.log(a());
+// console.log(b());
+
+//For loop
+var sum =0;
+for(var i=0; i<=10; i++){
+	console.log(i);
+	sum+=i;
+}
+console.log("sum from 0 to 9 is : ", sum);
