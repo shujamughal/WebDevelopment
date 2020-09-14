2ee72bd7517bc72b16337063addb3fc5dbd09d7f
shujamughal <shujamughal@gmail.com>
2020-09-14 16:23:29 +0500

    27

--- Lecture27/js/js27.js
+++ Lecture27/js/js27.js
@@ -1,1 +1,42 @@
 //Closures
+
+
+// function myFunction(){
+	// var name= "baig"; //local variable
+	
+	// function displayName(){ //inner function
+		// console.log(name);
+	// }
+	
+	// displayName();
+// }
+
+// myFunction();
+
+// function myFunction(){
+	// var name= "baig"; //local variable
+	
+	// function displayName(){ //inner function
+		// console.log(name);
+	// }
+	
+	// return displayName;
+// }
+
+// var func =  myFunction();
+
+// func();
+
+function makeAdder(x){
+	
+	return function(y){
+		return x+y;
+	};
+	
+}
+
+var add5 =makeAdder(5);
+var add10 = makeAdder(10);
+
+console.log(add5(2));
+console.log(add10(2));
